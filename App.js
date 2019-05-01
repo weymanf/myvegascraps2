eb821d078ffc521fe5e3d9e4fad08569f2be3d92
Weyman Fung <wfung@pivotal.io>
2019-04-30 19:37:50 -0700

    add navigation

--- App.js
+++ App.js
@@ -9,6 +9,8 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
 import Table from './src/app/component/table'
+import Home from './src/app/component/home'
+import {createStackNavigator, createAppContainer} from 'react-navigation';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,34 +19,15 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
+const MainNavigator = createAppContainer(createStackNavigator({
+	Home: {screen: Home},
+	Table: {screen: Table},
+}));
+
 export default class App extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-        <Table></Table>
-      </View>
+      <MainNavigator></MainNavigator>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
---
+++ scripts/dev-clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+rm -rf /tmp/metro-bundler-cache-*
+rm -rf /tmp/haste-map-react-native-packager-*
+rm -rf node_modules && yarn install
